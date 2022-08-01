44a5393e192714ad284bd8f4e5167109e7e44896
Enrique Teran <eteran9669@gmail.com>
2022-08-01 16:07:52 -0500

    Fix vol5

--- Color.c
+++ Color.c
@@ -2,6 +2,26 @@
 #include <assert.h>
 #include "Color.h"
 
+
+const char* MajorColorNames[] = {
+    "White", "Red", "Black", "Yellow", "Violet"
+};
+int numberOfMajorColors =
+    sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+
+const char* MinorColorNames[] = {
+    "Blue", "Orange", "Green", "Brown", "Slate"
+};
+const int MAX_COLORPAIR_NAME_CHARS = 16;
+
+int numberOfMinorColors =
+    sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+
+typedef struct {
+    enum MajorColor majorColor;
+    enum MinorColor minorColor;
+} ColorPair;
+
 ColorPair GetColorFromPairNumber(int pairNumber) {
     ColorPair colorPair;
     int zeroBasedPairNumber = pairNumber - 1;
